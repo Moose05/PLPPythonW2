2ff93ec285e69a05b80c4f64f6b1eaaf6ab4e1c5
Moose05 <69054872+Moose05@users.noreply.github.com>
2025-04-18 16:25:16 +0200

    Create PlpWeek2-Python.py
    Power Learn Project Week 2 Assignment Complete

---
+++ PlpWeek2-Python.py
@@ -0,0 +1,19 @@
+#Musa Mnanzana Week 2 Assignment
+
+my_list= [] #1 Create an empty list called my_list
+
+my_list2 = [10,20,30,40] #2 Append the following items to the list (10, 20, 30,40 )
+
+my_list3= [50,60,70] #4 Extend the list with another list (50. 60, 70)
+
+my_list.extend(my_list2) 
+
+my_list.insert(1,15) #3 Insert 15 at the second position in the list
+
+my_list. extend(my _list3)
+
+del my_list[7] #5 Remove the last element from my_list
+
+my_list.sort() #6 Sort my_list in ascending order
+
+print(my_list[3]) #7. Find and print the index of the value of 30 in my_list
